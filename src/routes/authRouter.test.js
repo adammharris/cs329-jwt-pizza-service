4d9103a01ca93640e55afae042521084b7e88aec
adammharris <97053407+adammharris@users.noreply.github.com>
2025-09-23 16:07:48 -0600

    add first tests

---
+++ src/routes/authRouter.test.js
@@ -0,0 +1,42 @@
+const request = require("supertest");
+const app = require("../service");
+
+const testUser = { name: "pizza diner", email: "reg@test.com", password: "a" };
+let testUserAuthToken;
+
+beforeAll(async () => {
+  testUser.email = Math.random().toString(36).substring(2, 12) + "@test.com";
+  const registerRes = await request(app).post("/api/auth").send(testUser);
+  testUserAuthToken = registerRes.body.token;
+  expectValidJwt(testUserAuthToken);
+});
+
+test("login", async () => {
+  const loginRes = await request(app).put("/api/auth").send(testUser);
+  expect(loginRes.status).toBe(200);
+  expectValidJwt(loginRes.body.token);
+
+  const expectedUser = { ...testUser, roles: [{ role: "diner" }] };
+  delete expectedUser.password;
+  expect(loginRes.body.user).toMatchObject(expectedUser);
+});
+
+test("register", async () => {
+  const registerRes = await request(app).post("/api/auth").send(testUser);
+  expect(registerRes.status).toBe(200);
+  expectValidJwt(registerRes.body.token);
+
+  const expectedUser = { ...testUser, roles: [{ role: "diner" }] };
+  delete expectedUser.password;
+  expect(registerRes.body.user).toMatchObject(expectedUser);
+});
+
+test("logout", async () => {
+  expect("unimplemented" == 0);
+});
+
+function expectValidJwt(potentialJwt) {
+  expect(potentialJwt).toMatch(
+    /^[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*$/,
+  );
+}
---
+++ src/routes/franchiseRouter.test.js
@@ -0,0 +1,6 @@
+//const request = require("supertest");
+//const app = require("../service");
+
+test("hello world", async () => {
+  expect("hello " + "world" == "hello world");
+});
